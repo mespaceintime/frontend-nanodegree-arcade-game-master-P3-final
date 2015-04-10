b8c167568531917fbf73c23440820cd3dc5ef43d
mespaceintime <mespaceintime@gmail.com>
2015-04-10 11:17:50 -0400

    Commit1

---
+++ README.md
@@ -0,0 +1,48 @@
+frontend-nanodegree-arcade-game
+===============================
+
+Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
+
+for self-checking their submission.
+
+The Player must cross the road to get to the water.
+The Bug-Enemy will try to stop you from crossing the road by bumping into you.  When the Bug-Enemy bumps you, you die and restart on from the grass.  After 3 lives/restarts the game is over.
+When you reach the water you win.
+
+This project provided experience with object-oriented Javascript and the HTML5 Canvas.
+
+**Associated Udacity courses include:**
+
+* [Object-Oriented Javascript](https://www.udacity.com/course/ud015)
+* [HTML5 Canvas](https://www.udacity.com/course/ud292)
+
+**Goal of the Project:**
+
+* In this project you will write the JavaScript that powers your own interactive resume.
+
+*This project is meant to be completed while taking the JavaScript Basics course. Throughout the course you'll build your resume by writing a JS script that will combine your personal information with pre-written HTML and CSS templates to generate your resume.
+
+**Results:**
+
+An error free resume with all the basic requirements meet.
+
+###Resources Used:
+
+**Primary Courses**
+
+* [JavaScript Basics](https://www.udacity.com/course/viewer#!/c-ud804-nd)
+* [Intro to jQuery](hhttps://www.udacity.com/course/viewer#!/c-ud245-nd)
+* [HTML5 Game Development](https://www.udacity.com/course/viewer#!/c-cs255/l-52265917/e-130215279/m-129941630)
+* [JavaScript Loops, Arrays and Objects](http://teamtreehouse.com/library/javascript-loops-arrays-and-objects)
+
+**Additional Resources**
+
+* [Udacity Discussion Forum](http://discussions.udacity.com/)
+* [Piazza Forums](https://piazza.com/class/i36sqlrb9xu332)
+* [Google Search "js validator"](https://www.google.com/webhp?sourceid=chrome-instant&ion=1&espv=2&ie=UTF-8#q=js+validator)
+* JAVASCRIPT & JQUERY - John Duckett
+* Thanks you to Udacity, my classmates Mikhak Misaghian, Ben Plant and others for all your help!
+* [jslint](http://jslint.com/)
+* [Google JavaScript Tutorial](https://developers.google.com/maps/documentation/javascript/tutorial)
+* [jshint](http://jshint.com/)
+* Learning Web Design - Jennifer Niederst Robbins
---
+++ js/app.js
@@ -0,0 +1,139 @@
+/* ******* Variables ******* */
+var currentPlayer;
+var Player;
+var x;
+var y;
+var speed;
+var Enemy;
+var dist;
+var begin = false;
+var player1 = false;
+
+/* ******* Enemies ******* */
+// Enemies our player must avoid
+Enemy = function () {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    'use strict';
+    this.sprite = 'images/enemy-bug.png';
+
+    this.row = Math.random() * 400;
+    this.column = 70 + (Math.floor(Math.random() * 2) * 85);
+    this.speed = (Math.random() * 110) + 85;
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    dist = dt * this.speed;
+    this.row += dist;
+
+    if (this.row > 400) {
+        this.row = -55;
+    }
+
+//Defining the edges of the enemy for collision detection
+    this.top = this.column;
+    this.left = this.row;
+    this.right = this.row + 55;
+    this.bottom = this.column + 55;
+
+    if(player1) {
+        this.checkCollision(this, player);
+    }
+};
+
+//Player enemy intersection
+Enemy.prototype.intersectRect = function(r1, r2) {
+        return !(r1.top > r2.bottom || r1.left > r2.right || r1.right < r2.left || r1.bottom < r2.top);
+};
+
+//Player enemy collision, reset player
+Enemy.prototype.checkCollision = function(r1, r2) {
+    if(this.intersectRect(r1, r2)) {
+            player.resetPosition();
+    }
+};
+
+//Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {  
+    ctx.drawImage(Resources.get(this.sprite), this.row, this.column);
+};
+
+/* ******* Player ******* */
+Player = function(x, y) {
+    // Initializing
+    this.sprite = 'images/char-princess-girl.png';
+    this.row = x;
+    this.column = y;
+};
+
+// Move player
+Player.prototype.update = function() {
+    player1 = true;
+    this.top = this.column;
+    this.left = this.row;
+    this.right = this.row + 55;
+    this.bottom = this.column + 55;
+};
+
+// Draw player 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.row, this.column);
+};
+
+// Player starting position
+Player.prototype.resetPosition = function() {
+    this.row = 400;
+    this.column = 400;
+};
+
+Player.prototype.handleInput = function(key) {
+    if (key === 'left') {
+        if (this.row > 55) {
+            this.row = this.row - 100;
+        }
+    }
+    if (key === 'up') {
+        if (this.column > 100) {
+            this.column = this.column - 55;
+        } else {
+            this.resetPosition();
+        }
+    }
+    if (key === 'right') {
+        if (this.row < 400) {
+            this.row = this.row + 100;
+        }
+    }
+    if (key === 'down') {
+        if (this.column < 400) {
+            this.column = this.column + 55;
+        }
+    }
+};
+
+var bug1 = new Enemy(120,120);
+var bug2 = new Enemy(220,220);
+var bug3 = new Enemy(320,320);
+var allEnemies = [bug1, bug2, bug3];
+var player = new Player(400, 400);
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
